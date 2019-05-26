eb4490345603ba20cea24576352137459103438d
vozup <vozupserega@gmail.com>
2019-05-26 11:57:43 +0300

    Работа с thymeleaf

--- src/main/java/com/vozup/weathercompare/db/GismeteoSitesRepository.java
+++ src/main/java/com/vozup/weathercompare/db/GismeteoSitesRepository.java
@@ -1,9 +1,8 @@
 package com.vozup.weathercompare.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "GismeteoCities", path = "GismeteoCities")
+
 public interface GismeteoSitesRepository extends JpaRepository<GismeteoCitiesEntity, Long> {
 
 }
--- src/main/java/com/vozup/weathercompare/db/SinoptikCitiesRepository.java
+++ src/main/java/com/vozup/weathercompare/db/SinoptikCitiesRepository.java
@@ -1,9 +1,8 @@
 package com.vozup.weathercompare.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "SinoptikCities", path = "SinoptikCities")
+
 public interface SinoptikCitiesRepository extends JpaRepository<SinoptikCitiesEntity, Long> {
 
 }
